7744b80ea962d9d6ccd50cdfe5a912ddf3af8fe4
wangyaleiit <764198025@qq.com>
2018-02-27 16:18:19 +0800

    1.0

--- FrontBlog/src/components/ArticlesInfo.vue
+++ FrontBlog/src/components/ArticlesInfo.vue
@@ -8,18 +8,18 @@
 					<i class="mdui-icon material-icons" style="font-size: 16px;">&#xe916;</i>&nbsp;{{art.updateDate}}&emsp;
 				</div>
 		    </div>
-		    <div class="mdui-card-content mlist"> 
-		    	{{art.mdcontent}}
+		    <div class="mdui-card-content" > 
+		    	 <div style="text-align: left" v-html="art.htmlcontent" v-highlight></div>
 		    </div>
 		    <div class="mdui-divider-inset-dark" style="margin:10px 20px"></div>
 		    <div class="mdui-card-primary" style="padding: 10px 24px;">
 			    <div class="mdui-card-primary-subtitle">
-						<i class="mdui-icon material-icons" style="font-size: 14px;">&#xe54e;</i>{{art.topicName}}
+					<i class="mdui-icon material-icons" style="font-size: 14px;">&#xe54e;</i>&emsp;{{art.topicName}}
 		    	</div>
 		    </div>
 		</div>
 		<div class="mdui-float-left" v-if="preArt !== null">
-			<button class="mdui-btn  mdui-btn-icon mdui-btn-dense mdui-color-blue-grey mdui-ripple" @click="preHand(preArt.id)">
+			<button class="mdui-btn  mdui-btn-icon mdui-btn-dense mdui-color-light-blue-a700N mdui-ripple" @click="preHand(preArt.id)">
 				<li class="mdui-icon material-icons ">&#xe408;</li>
 			</button>&emsp;{{preArt.title}}
 		</div>
@@ -57,7 +57,6 @@ export default {
 		getRequest('/front/queryByKey/'+ id).then(resp=>{
 			if (resp.data.success) {
 				this.art = resp.data.result.art
-				console.log('resp.data.result.preArt' + resp.data.result.preArt)
 			    this.preArt = resp.data.result.preArt
 				this.nextArt = resp.data.result.nextArt
 				this.loading = false
@@ -69,4 +68,44 @@ export default {
 </script>
 
 <style>
+blockquote{
+  display:block;
+  background: #fff;
+  padding: 15px 20px 15px 45px;
+  margin: 0 0 20px;
+  position: relative;
+ 
+  /*字体*/
+  font-family: Georgia, serif;
+  font-size: 16px;
+  line-height: 1.2;
+  color: #666;
+  text-align: justify;
+  
+  /*边框 - (选项)*/
+  border-left: 15px solid #c76c0c;
+  border-right: 2px solid #c76c0c;
+ 
+  /*盒子阴影 - (选项)*/
+  -moz-box-shadow: 2px 2px 15px #ccc;
+  -webkit-box-shadow: 2px 2px 15px #ccc;
+  box-shadow: 2px 2px 15px #ccc;
+}
+blockquote::before{
+  content: "\201C"; /*左双引号的Unicode编码*/
+ 
+  /*字体*/
+  font-family: Georgia, serif;
+  font-size: 60px;
+  font-weight: bold;
+  color: #999;
+ 
+  /*位置*/
+  position: absolute;
+  left: 10px;
+  top:5px;
+}
+blockquote::after{
+  content: ""; /*如果要显示右双引号，则写 content: "\201D"; */
+}
 </style>
