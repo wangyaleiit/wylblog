9b6d833017242c6e61f30b16722eb910c40c87d5
wangyaleiit <764198025@qq.com>
2018-02-28 14:31:01 +0800

    1.0

---
+++ FrontBlog/src/components/TimeLine.vue
@@ -0,0 +1,39 @@
+<template>
+    <Timeline>
+        <Timeline-item>
+            <p class="time">1976年</p>
+            <p class="content">Apple I 问世</p>
+        </Timeline-item>
+        <Timeline-item>
+            <p class="time">1984年</p>
+            <p class="content">发布 Macintosh</p>
+        </Timeline-item>
+        <Timeline-item>
+            <p class="time">2007年</p>
+            <p class="content">发布 iPhone</p>
+        </Timeline-item>
+        <Timeline-item>
+            <p class="time">2010年</p>
+            <p class="content">发布 iPad</p>
+        </Timeline-item>
+        <Timeline-item>
+            <p class="time">2011年10月5日</p>
+            <p class="content">史蒂夫·乔布斯去世</p>
+        </Timeline-item>
+    </Timeline>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .time{
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .content{
+        padding-left: 5px;
+    }
+</style>
